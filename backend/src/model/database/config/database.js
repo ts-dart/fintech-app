b7690f38536b728cb4329319a1e2e4acba9344e7
Thiago Henrique <68298881+ts-dart@users.noreply.github.com>
2024-05-01 23:20:37 -0300

    Update database.js

--- backend/src/model/database/config/database.js
+++ backend/src/model/database/config/database.js
@@ -1,7 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
 createClient(
-  'https://db.djxfqwkvdszmvybktbmc.supabase.co', 
+  'https://djxfqwkvdszmvybktbmc.supabase.co', 
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqeGZxd2t2ZHN6bXZ5Ymt0Ym1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODgxNzg1ODAsImV4cCI6MjAwMzc1NDU4MH0.jvuvLb5I9HyMS90dF5dUEEle23Oc71aCiJwl9cFx5wk'
 );
 
