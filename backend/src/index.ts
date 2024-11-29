63cbb45c56c88698e7a6279f5a25d236dec39b26
Thiago <thiagoedusan5.11@outlook.com>
2024-11-29 13:52:22 -0300

    ...

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,6 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get('/teste', (_req, res) => res.status(200).json('rodando'))
 app.use('/register', router.registerRouter);
 app.use('/login', router.loginRouter);
 app.use('/balance', router.balanceRouter);
