fb3057240e3c1b43eb3e9cac73bfc089a6b22ac3
Thiago <thiagoedusan5.11@outlook.com>
2024-11-29 02:42:14 -0300

    ...

--- backend/src/model/database/config/database.js
+++ backend/src/model/database/config/database.js
@@ -4,6 +4,7 @@ const config = {
   username: process.env.DB_USENAME,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
   dialect: 'postgres',
   port: process.env.DB_PORT,
 }
