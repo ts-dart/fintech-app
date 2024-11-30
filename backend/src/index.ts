537b8c463bb86ee61a7c2e020e1346f6b62c276e
Thiago Henrique <68298881+ts-dart@users.noreply.github.com>
2024-11-30 00:23:25 -0300

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/teste', (_req, res) => res.status(200).json('rodando'))
+app.get('/teste', (_req, res) => res.status(200).json('API responding))
 app.use('/register', router.registerRouter);
 app.use('/login', router.loginRouter);
 app.use('/balance', router.balanceRouter);
